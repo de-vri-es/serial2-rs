b6927660db5572d9b0faa2e080bbfc3be6aca972
Maarten de Vries <maarten@de-vri.es>
2021-11-07 14:49:26 +0100

    Fix compilation for illumos.

---
+++ src/sys/unix/fills/illumos.rs
@@ -0,0 +1,21 @@
+// All values taken from:
+// https://github.com/illumos/illumos-gate/blob/252adeb303174e992b64771bf9639e63a4d55418/usr/src/uts/common/sys/termios.h
+
+#[allow(non_upper_case_globals)]
+const tIOC: u32 = 0x7400; // 't' << 8;
+
+//pub const TIOCMSET: u32 = tIOC | 26;
+pub const TIOCMBIS: u32 = tIOC | 27;
+pub const TIOCMBIC: u32 = tIOC | 28;
+pub const TIOCMGET: u32 = tIOC | 29;
+//pub const TIOCM_LE: i32 = 0001;
+pub const TIOCM_DTR: i32 = 0002;
+pub const TIOCM_RTS: i32 = 0004;
+//pub const TIOCM_ST: i32 = 0010;
+//pub const TIOCM_SR: i32 = 0020;
+pub const TIOCM_CTS: i32 = 0040;
+pub const TIOCM_CAR: i32 = 0100;
+pub const TIOCM_CD: i32 = TIOCM_CAR;
+pub const TIOCM_RNG: i32 = 0200;
+pub const TIOCM_RI: i32 = TIOCM_RNG;
+pub const TIOCM_DSR: i32 = 0400;
--- src/sys/unix/fills/mod.rs
+++ src/sys/unix/fills/mod.rs
@@ -4,6 +4,9 @@ cfg_if! {
 	if #[cfg(any(target_os = "linux", target_os = "android"))] {
 		mod linux;
 		pub use linux::*;
+	} else if #[cfg(target_os = "illumos")] {
+		mod illumos;
+		pub use illumos::*;
 	} else {
 		mod other;
 		pub use other::*;
