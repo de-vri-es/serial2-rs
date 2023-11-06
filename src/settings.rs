10dfafe81e9ffaaba36a6e698a3d1bf97579495d
avocadochicken <avocadochicken@users.noreply.github.com>
2023-11-06 22:30:51 +0100

    Update src/settings.rs
    Co-authored-by: Maarten de Vries <maarten@de-vri.es>

--- src/settings.rs
+++ src/settings.rs
@@ -36,7 +36,7 @@ pub enum CharSize {
 
 /// The number of stop bits per character for a serial port.
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
-#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum StopBits {
 	/// One stop bit.
 	One,
